2a7b20a92606327f53ec7e4e7a630a9c6f9d6fe2
adityapsvs <adityapsvs@gmail.com>
2017-11-11 18:00:36 +0530

    Making the website responsive

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,28 +1,28 @@
-$(document).ready(() => {
-
-  function validateEmail(emailAddress) {
-      var regularExpression = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))){2,6}$/i;
-      // console.log(emailAddress);
-      // console.log(regularExpression.test(emailAddress));
-       return regularExpression.test(emailAddress);
-  }
-
-  function showEmailValidationState(event) {
-      console.log(event.target.value);
-      if (!validateEmail(event.target.value)) {
-        $("#formButton").prop("disabled",true);
-      }
-      else if(validateEmail(event.target.value)) {
-        $('#formButton').removeAttr('disabled');
-        // $('#downloadButton').removeAttr('disabled');
-      }
-  }
-
-  document.getElementById("formEmail").addEventListener("keyup", showEmailValidationState);
-  function downloadme(){
-    console.log('enje');
-    winObj = window.open(x,'','left=10000,screenX=10000');
-    winObj.document.execCommand('SaveAs','null','../VD_Company_Brochure.pdf');
-    winObj.close();
-  }
-})
+// $(document).ready(() => {
+//
+//   function validateEmail(emailAddress) {
+//       var regularExpression = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))){2,6}$/i;
+//       // console.log(emailAddress);
+//       // console.log(regularExpression.test(emailAddress));
+//        return regularExpression.test(emailAddress);
+//   }
+//
+//   function showEmailValidationState(event) {
+//       console.log(event.target.value);
+//       if (!validateEmail(event.target.value)) {
+//         $("#formButton").prop("disabled",true);
+//       }
+//       else if(validateEmail(event.target.value)) {
+//         $('#formButton').removeAttr('disabled');
+//         // $('#downloadButton').removeAttr('disabled');
+//       }
+//   }
+//
+//   document.getElementById("formEmail").addEventListener("keyup", showEmailValidationState);
+//   function downloadme(){
+//     console.log('enje');
+//     winObj = window.open(x,'','left=10000,screenX=10000');
+//     winObj.document.execCommand('SaveAs','null','../VD_Company_Brochure.pdf');
+//     winObj.close();
+//   }
+// })
